67c3d86b1ba43a9a69ee792f175540ffb53db9bf
Gabriel Seals <sealsgj@gmail.com>
2020-03-23 21:22:24 -0500

    Add project files.

---
+++ FavoritePizzaToppings/Pizza.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoritePizzaToppings
+{
+    class Pizza
+    {
+        public List<string> Toppings { get; set; }
+    }
+}
---
+++ FavoritePizzaToppings/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace FavoritePizzaToppings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pizzas = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"./pizzas.json"));
+
+            var toppingLists = pizzas.Select(p => string.Join(",", p.Toppings.OrderBy(t => t)));
+
+            //Distinct is a Linq method that gives us unique topping combinations
+            var distinctToppingCombinations = toppingLists.Distinct();
+
+            //int will be how many times this combination appears
+            var countOfCombination = new Dictionary<string, int>();
+
+            foreach (var combination in toppingLists)
+            {
+                if (!
+                    countOfCombination.ContainsKey(combination))
+                {
+                    countOfCombination.Add(combination, 1);
+                }
+                else
+                {
+                    countOfCombination[combination] += 1;
+                }
+            }
+            var mostOrdered = countOfCombination.OrderByDescending(item => item.Value).Take(20);
+
+            foreach (var (combination, count) in mostOrdered)
+            {
+                Console.WriteLine($"The topping combination of {combination} was ordered {count} times.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
